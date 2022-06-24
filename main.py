979387a73971a89b6ed2b575ff60a27e0407306b
fleetster <58215141+fleetster22@users.noreply.github.com>
2022-06-23 22:38:22 -0400

    more practice - focus on bs vs regex

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import re
 
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
+url = "https://google-gruyere.appspot.com/"
 page = urlopen(url)
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+html = page.read()
+soup = BeautifulSoup(html, "html.parser")
 
-print(html)     #test to see if you get the page
 
-title_index = html.find("<title>") #returns index of title tag
-start_index = title_index + len("<title>") #returns index of the actual title
-end_index = html.find("</title>") #returns index of closing title tag
-title = html[start_index:end_index] #returns title
-hint_index = html.find("hint")
+# print(soup.get_text())
 
-print("Website title: ", title)
 
-w = re.findall("hidden", html, re.IGNORECASE)
-x = re.findall("hint", html, re.IGNORECASE)
-y = re.findall("flag", html, re.IGNORECASE)
-z = re.findall("animal", html, re.IGNORECASE)
-print(hint_index)
+for link in soup.find_all("a"):
+   print(link.get("href"))
 
-print(w)
-print(x)
-print(y)
-print(z)
+images = soup.find_all('img')
+for img in images:
+    if img.has_attr('src'):
+        print(img['src'])
+
+# words = soup.find_all('html', ['secret', 'hint', 'password', 'none'])
+
+words = ['secret', 'hint', 'password', 'none']
+for word in words:
+    print(word, len(soup.find_all(text=lambda text: text and word in text)))
+
+# HTML FORMS
+
+
+
+
+
+
+
+
+
